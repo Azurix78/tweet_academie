b91ab9e76b1a2b4516b9698ec9c79e0ead03479a
happy1337 <happy-666@hotmail.fr>
2013-06-28 14:08:11 +0200

    wall

--- inc/wall.php
+++ inc/wall.php
@@ -1,3 +1,108 @@
-<div class="container wall_bloc">
-	
+<div class="container body-complete">
+	<div class="left">
+		<div class="bloc wall-profil">
+			<div class="imgfullname">
+				<img src="upload/img/nico.png" alt="avatar">
+				<span class="fullname">
+					<b>RIVIERE Nicolas</b>
+					<a href="#">Voir ma page de profil</a>
+				</span>
+			</div>
+				<form method="POST" class="newtweet">
+					<input type="text" name="new-tweet" placeholder="Ecrire un nouveau tweet...">
+					<input type="submit" name="bouton-newtweet">
+				</form>
+		</div>
+		<div class="bloc wall-menu">
+			<ul>
+				<li><a href=""><i class="icon-comment"></i>Mentions</a></li>
+				<li><a href=""><i class="icon-envelope"></i>Messages privés</a></li>
+				<li><a href=""><i class="icon-list"></i>Listes</a></li>
+				<li><a href=""><i class="icon-signal"></i>Activité du réseau</a></li>
+				<li class="fin-li"><a href=""><i class="icon-calendar"></i>A la une</a></li>
+			</ul>
+		</div>
+		<div class="bloc wall-footer">
+			<ul>
+				<li>&copy; 2013 Swiffer</li>
+				<li><a href="">A propos</a></li>
+				<li><a href="">Aide</a></li>
+				<li><a href="">Conditions</a></li>
+				<li><a href="">Confidentialité</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Statut</a></li>
+				<li><a href="">Applications</a></li>
+				<li><a href="">Ressources</a></li>
+				<li><a href="">Offre d'emploi</a></li>
+				<li><a href="">Annonceurs</a></li>
+				<li><a href="">Professionels</a></li>
+				<li><a href="">Médias</a></li>
+				<li><a href="">Développeurs</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="right">
+		<div class="bloc wall-tweets">
+			<h4 class="tweets">Tweets</h4>
+			<ul>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+		
+	</div>
 </div>
