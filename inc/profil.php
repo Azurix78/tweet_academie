4100cf42efa9145dbf2c3891446cea2148632c4c
Azurix78 <ni.rubio78@gmail.com>
2013-07-01 17:27:04 +0200

    profil_integration

---
+++ inc/profil.php
@@ -0,0 +1,237 @@
+<?php
+
+
+
+?>
+<div class="container body-complete">
+	<div class="left">
+		<div class="bloc wall-menu">
+			<ul>
+				<li><a href="">Tweets<span class="menu-chev"><i class="icon-arrow-right"></i></span></a></li>
+				<li><a href="">Abonnements<span class="menu-chev"><i class="icon-arrow-right"></i></span></a></li>
+				<li><a href="">Abonnés<span class="menu-chev"><i class="icon-arrow-right"></i></span></a></li>
+				<li><a href="">Favoris<span class="menu-chev"><i class="icon-arrow-right"></i></span></a></li>
+				<li><a href="">Listes<span class="menu-chev"><i class="icon-arrow-right"></i></span></a></li>
+			</ul>
+		</div>
+		<div class="bloc wall-menu">
+			<ul>
+				<li><a href=""><i class="icon-comment"></i>Mentions</a></li>
+				<li><a href=""><i class="icon-envelope"></i>Messages privés</a></li>
+				<li><a href=""><i class="icon-list"></i>Listes</a></li>
+				<li><a href=""><i class="icon-signal"></i>Activité du réseau</a></li>
+				<li class="fin-li"><a href=""><i class="icon-calendar"></i>A la une</a></li>
+			</ul>
+		</div>
+		<div class="bloc wall-footer">
+			<ul>
+				<li>&copy; 2013 Swiffer</li>
+				<li><a href="">A propos</a></li>
+				<li><a href="">Aide</a></li>
+				<li><a href="">Conditions</a></li>
+				<li><a href="">Confidentialité</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Statut</a></li>
+				<li><a href="">Applications</a></li>
+				<li><a href="">Ressources</a></li>
+				<li><a href="">Offre d'emploi</a></li>
+				<li><a href="">Annonceurs</a></li>
+				<li><a href="">Professionels</a></li>
+				<li><a href="">Médias</a></li>
+				<li><a href="">Développeurs</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="right ban-profil">
+		<div class="ban-info">
+			<img class="avatar-ban" src="img/avatar.png" alt="avatar">
+			<div class="text_ban">
+				<h1>USERNAME</h1>
+				<h2>@htags_user</h2>
+				<p>Locacity</p>
+			</div>
+		</div>
+		<div class="ban-nav">
+			<ul class="inline link-nav">
+				<li><a href=""><p><strong>202</strong>Tweet</p></a></li>
+				<li><a href=""><p><strong>202</strong>Abonnement</p></a></li>
+				<li><a href=""><p><strong>202</strong>Abonn&eacute;</p></a></li>
+			</ul>
+			<ul class="inline btn-nav">
+				<li><button class="btn"><i class="icon-envelope"></i></button></li>
+				<li><button class="btn">Editer le profil</button></li>
+			</ul>
+		</div>
+
+	</div>
+
+
+	<div class="right">
+		<div class="bloc wall-tweets">
+			<h4 class="tweets">Tweets</h4>
+			<ul>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li>
+					<div class="imgtweets">
+						<img src="upload/img/nico.png" alt="avatar">
+					</div>
+					<div class="tweet">
+						<b>RIVIERE Nicolas</b>
+						<span>@Happy_Portfolio</span>
+						<span class="date-tweet">16 Jun</span>
+						<p>Bonjour à tous,<br>
+
+						10 nouveaux jeux pour vos serveurs sont installable/gérable sur vos serveurs dédiés OVH avec JSI... http://fb.me/1Inzk8TA0 </p>
+						<a href="#" class="open-tweet">Ouvrir</a>
+					</div>
+				</li>
+				<li id="back">
+					<div class="div-back">
+						<img class="back-top" src="img/back-logo.png" alt="logo retour en haut"><br>
+						<a href="#" onClick="scrolTop();" class="back-toptext">Retour en haut ↑</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+		
+	</div>
+</div>
