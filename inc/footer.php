a0acace2f9907a59e6f1faa880aa7c914b63804a
Rubio <ni.rubio78@gmail.com>
2013-07-06 00:51:13 +0200

    recherche et liste membre

--- inc/footer.php
+++ inc/footer.php
@@ -4,5 +4,6 @@
 	<script src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="jscolor/jscolor.js"></script>
 	</body>
 </html>
