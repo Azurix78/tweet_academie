ac68afa3efefd8c5c2461346cfa15196a95f2b87
Fuck You <sir.winn3r@gmail.com>
2013-07-07 17:36:56 +0200

    fix upload image tweet

--- inc/functions.php
+++ inc/functions.php
@@ -134,4 +134,22 @@ function supbgimg($id)
    }
 }
 
+function curl_get_result($url)
+{
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+function bitly($url, $login, $appkey, $format='txt')
+{
+    $connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;
+    return curl_get_result($connectURL);
+}
+
 ?>
